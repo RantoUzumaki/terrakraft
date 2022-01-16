6500e10414f65bb522ca4f8b7c74ca94aea73b91
Wolfeinstein <rberk2011@gmail.com>
2022-01-16 22:17:26 +0530

    nil

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
             <Router>
                 <Header />
                 <Routes>
-                    <Route path="/" exact element={<Homepage />}></Route>
-                    <Route path="/login" exact element={<Login />}></Route>
+                    {/* <Route path="/" exact element={<Homepage />}></Route> */}
+                    <Route path="/" exact element={<Login />}></Route>
                     <Route path="/register" exact element={<Regitser />}></Route>
                     <Route
                         path="/single-product"
